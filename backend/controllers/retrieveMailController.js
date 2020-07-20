c0a6f662a35f2e506f1a0eeeacf1e11d58d45958
Alejandro Isa <alejandroisa71@gmail.com>
2020-07-20 19:05:59 -0300

    se modifica controladeres en emailcontroller, petController y retrieveMailController

--- backend/controllers/retrieveMailController.js
+++ backend/controllers/retrieveMailController.js
@@ -6,8 +6,7 @@ const userModel = require("../models/user");
 const retrieveController = {
   createUser1: async (req, res, next) => {
     const { email } = req.body;
-    const user = await userModel.findOne({email);
-    console.log(email);
+    const user = await userModel.findOne({ email });
     if (user) {
       res.status(404).send({
         error: `El email ${email} ya se encuentra en uso`,
